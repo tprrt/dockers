5602390b645157c6f3f8aee6149a9d255c64b87e
Thomas Perrot <thomas.perrot@tupi.fr>
2017-04-28 20:14:18 +0200

    Align ubuntu-toaster-env entrypoint.sh with that used for ubuntu-builder-env
    Signed-off-by: Thomas Perrot <thomas.perrot@tupi.fr>

--- ubuntu-toaster-env/entrypoint.sh
+++ ubuntu-toaster-env/entrypoint.sh
@@ -5,16 +5,28 @@ export USE_CCACHE=1
 export CCACHE_DIR=${HOME}/.ccache
 export PATH="/usr/lib/ccache/:${PATH}"
 
+# Add user into kvm group
+KVM_GID=${LOCAL_KVM_ID:-9002}
+
+if [ -z $(sed -nr "s/^kvm:x:([0-9]+):.*/\1/p" /etc/group) ] ; then
+    groupadd --system --gid ${KVM_GID} kvm || { echo "Unable to create the KVM group to the system" ; exit 1 ; }
+fi
+
 # Add local user
 USER_ID=${LOCAL_USER_ID:-9001}
 USER_NAME=${LOCAL_USER_NAME:-user}
-HOME=/home/${USER_NAME}
-GROUPS="users sudo dialout"
+USER_GROUPS="users,sudo,dialout,kvm"
+USER_HOME=/home/${USER_NAME}
 
-echo "Starting with UID : '${USER_ID}' and name '${USER_NAME}'"
-
-useradd --shell /bin/bash -u ${USER_ID} -o -c "" -m ${USER_NAME} --home ${HOME} --groups ${GROUPS} \
+useradd --shell /bin/bash -u ${USER_ID} -m ${USER_NAME} --home ${USER_HOME} --groups ${USER_GROUPS} \
 	|| { echo "Unable to add the local user" ; exit 1 ; }
+
+
+# Edit sudoer to add NOPASSWD attribut at the user permission
+echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers || { echo "Unable to edit sudoer" ; exit 1 ; }
+
+# Switch to the local user
+su - ${USER_NAME} || { echo "Unable to swith to the local user" ; exit 1 ; }
 
 # Download source
 URI=${URI:-git@github.com:tprrt/exiguous-manifest.git}
